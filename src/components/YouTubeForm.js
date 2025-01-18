12e91cb77b5632317933750a86e2c233ba1d971d
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2025-01-18 22:14:27 +0100

    form

--- src/components/YouTubeForm.js
+++ src/components/YouTubeForm.js
@@ -3,7 +3,10 @@ import React from 'react'
 const YouTubeForm = () => {
   return (
     <div>
-        
+        <form>
+            <label htmlFor='username'>Username</label>
+            <input type='text' id='username' name='username'/>
+        </form>
     </div>
   )
 }
